c68bb945b9b82bf0cc6dfaa5c0419a7b3941ce12
root <kmeans.shong@gmail.com>
2019-12-26 14:49:02 +0800

    add a script to learning alpha zero

---
+++ MuZero/muzero.py
@@ -0,0 +1,52 @@
+
+# saving a version of the neural network and retrieving the latest neural network from the store.
+class SharedStorage(object):
+
+  def __init__(self):
+    self._networks = {}
+
+  def latest_network(self) -> Network:
+    if self._networks:
+      return self._networks[max(self._networks.keys())]
+    else:
+      # policy -> uniform, value -> 0, reward -> 0
+      return make_uniform_network()
+
+  def save_network(self, step: int, network: Network):
+    self._networks[step] = network
+
+# store data from previous games. This takes the following form
+class ReplayBuffer(object):
+
+  def __init__(self, config: MuZeroConfig):
+    self.window_size = config.window_size
+    self.batch_size = config.batch_size
+    self.buffer = []
+
+  def save_game(self, game):
+    if len(self.buffer) > self.window_size:
+      self.buffer.pop(0)
+    self.buffer.append(game)
+
+
+# Each self-play job is independent of all others; it takes the latest network
+# snapshot, produces a game and makes it available to the training job by
+# writing it to a shared replay buffer.
+def run_selfplay(config: MuZeroConfig, storage: SharedStorage,
+                 replay_buffer: ReplayBuffer):
+  while True:
+    network = storage.latest_network()
+    game = play_game(config, network)
+    replay_buffer.save_game(game)
+
+
+def muzero(config: MuZeroConfig):
+    storage = SharedStorage()
+    replay_buffer = ReplayBuffer(config)
+
+    for _ in range(config.num_actors):
+        launch_job(run_selfplay, config, storage, replay_buffer)
+
+    train_network(config, storage, replay_buffer)
+
+    return storage.latest_network()
